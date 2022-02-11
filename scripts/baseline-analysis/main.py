0122e22b0b04256f4dbadad826bb8dc1b27371fa
Schmid Marco <marco.schmid@fhgr.ch>
2022-02-11 13:50:17 +0100

    small corrections

--- scripts/baseline-analysis/main.py
+++ scripts/baseline-analysis/main.py
@@ -4,6 +4,6 @@ import final_train_model
 
 if __name__ == '__main__':
 
-    # final_preprocessing.run()
-    # final_eda.run()
+    final_preprocessing.run()
+    final_eda.run()
     final_train_model.run()
